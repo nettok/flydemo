19eea840c9c7fd53b6a3bf3e94eea0374b5f7e65
Ernesto Menéndez <pyalec@gmail.com>
2025-07-26 17:03:54 -0600

    add a basic test for the existing hello world app (to test the CI/CD pipeline)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,48 @@
 use axum::response::Html;
-use axum::Router;
 use axum::routing::get;
+use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
-
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
 
     println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app()).await.unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
+fn app() -> Router {
+    Router::new()
+        .route("/", get(handle_index))
+}
+
+async fn handle_index() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn index() {
+        let app = app();
+
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"<h1>Hello, World!</h1>");
+    }
 }
